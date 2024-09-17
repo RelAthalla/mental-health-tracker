73ea50ab2ef60fd85fb04b870f503c714d79e5f1
RelAthalla <farrelathalla0910@gmail.com>
2024-09-18 01:26:44 +0700

    ganti yml sama yg baru

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -124,4 +124,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://farrel-athalla-mentalhealthtracker.pbp.cs.ui.ac.id/"
-                        , "https://farrel-athalla-mentalhealthtracker.pbp.cs.ui.ac.id/"]
+                        , "https://farrel-athalla-mentalhealthtracker.pbp.cs.ui.ac.id/"] 
