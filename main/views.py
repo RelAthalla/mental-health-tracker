5caa0d525e6fee3c42ea96e6049d51a2dd5b0baa
RelAthalla <farrelathalla0910@gmail.com>
2024-09-18 15:25:55 +0700

    coba login

--- main/views.py
+++ main/views.py
@@ -1,11 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.shortcuts import render
 from django.shortcuts import render, redirect
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
 from django.http import HttpResponse
 from django.core import serializers
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
     mood_entries = MoodEntry.objects.all()
 
@@ -13,7 +21,8 @@ def show_main(request):
         'name': 'Farrel Athalla Muljawan',
         'class': 'PBP E',
         'npm': '2306223925',
-        'mood_entries': mood_entries
+        'mood_entries': mood_entries,
+        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
@@ -27,6 +36,40 @@ def create_mood_entry(request):
 
     context = {'form': form}
     return render(request, "create_mood_entry.html", context)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        response = HttpResponseRedirect(reverse("main:show_main"))
+        response.set_cookie('last_login', str(datetime.datetime.now()))
+        return response
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
 
 def show_xml(request):
     data = MoodEntry.objects.all()
